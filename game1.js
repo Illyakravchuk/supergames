021fb5fcdba3d069fe0c68d8a92f6a2ab3633b28
Illyakravchuk <90210111+Illyakravchuk@users.noreply.github.com>
2022-06-05 17:50:34 +0300

    wrote a method that adds numbers to the cells

--- game1.js
+++ game1.js
@@ -29,4 +29,23 @@ class Game {
         }
         console.log(this.field);
     }
+
+    spawnUnit() {
+        let emptyCells = [];
+
+        for (let i = 0; i < this.field.length; i++) {
+            for (let p = 0; p < this.field[i].length; p++) {
+                if (!this.field[i][p].value) {
+                    emptyCells.push(this.field[i][p]);
+                }
+            }
+        }
+
+        if (emptyCells.length) {
+            emptyCells[randomInterval(0, emptyCells.length - 1)].spawn();
+        } else {
+            alert('You lose!');
+        }
+        // check if you have no empty cells left, then you lost
+    }
 }
