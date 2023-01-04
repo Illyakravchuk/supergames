4736bcb79d358793f0f2ba15f1e94ccb5ac40b2b
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-01-04 13:05:54 +0200

    added commits and 'use strict'

--- game_minesweeper/script.js
+++ game_minesweeper/script.js
@@ -1,50 +1,51 @@
+'use strict'
 startGame(8,8,15);
-function startGame(width, height ,bombsCount){
-    const field = document.querySelector('.field');
+function startGame(width, height ,bombsCount){         //the main function that starts all processes
+    const field = document.querySelector('.field'); //returns a page element based on an arbitrary CSS selector
     const cellCount = width * height;
-    field.innerHTML='<button></button>'.repeat(cellCount);
+    field.innerHTML='<button></button>'.repeat(cellCount); //generating buttons
     const cells = [...field.children];
 
 
-   const bombs = [...Array(cellCount).keys()]
+    const bombs = [...Array(cellCount).keys()]   //an array that transmits the coordinates of the generated bombs
         .sort(()=>Math.random() - 0.5)
         .slice(0,bombsCount);
 
-    field.addEventListener('click', (event)=> {
-       if (event.target.tagName !== 'BUTTON') {
-           return;
-       }
+    field.addEventListener('click', (event)=> {     //reacts to a click in the game area
+        if (event.target.tagName !== 'BUTTON') {
+            return;
+        }
 
-       const  index = cells.indexOf(event.target);
-       const column = index % width;
-       const row = Math.floor(index /width);
-       open(row, column)
+        const  index = cells.indexOf(event.target);
+        const column = index % width;
+        const row = Math.floor(index /width);
+        open(row, column)
 
     });
 
-    function getCount(row, column){
+    function getCount(row, column){         // counts the bombs nearby
         let count = 0;
         for(let x = -1; x <= 1; x++){
             for(let y = -1; y <= 1; y++){
-               if( isBomb(row + y,column + x)) {
-                   count++;
-               }
+                if( isBomb(row + y,column + x)) {
+                    count++;
+                }
             }
 
         }
         return count;
     }
-    function open(row, column){
+    function open(row, column){      // opens the cell and determines whether there is a bomb in the cell
         const index = row * width + column;
         const cell = cells[index];
         cell.innerHTML = isBomb(row, column) ? 'X' : getCount(row, column);
         cell.disabled = true;
     }
 
-       function isBomb(row, column) {
-           const index = row * width + column;
+    function isBomb(row, column) {     // determines if there is a bomb in this cell
+        const index = row * width + column;
 
-           return bombs.includes(index);
-       }
+        return bombs.includes(index);
+    }
 
 }
