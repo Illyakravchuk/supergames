898891b269d3593246a1c33c0eeb22584fef8e48
TangiresH <dimon4ik1010@gmail.com>
2023-01-06 16:12:07 +0200

    created an update function and setter and getter for the Paddle class

--- game_pong/paddle.js
+++ game_pong/paddle.js
@@ -1,6 +1,18 @@
+const SPEED = 0.02;
 export default class Paddle {
     constructor(paddleElem) {
         this.paddleElem = paddleElem;
     }
 
-}
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElem).getPropertyValue("--position"));
+    }
+
+    set position(value) {
+        this.paddleElem.style.setProperty("--position", value);
+    }
+
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position);
+    }
+};
