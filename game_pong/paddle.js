50077bf4724e52d4a0eaebd5b1391bde7fbba21b
TangiresH <dimon4ik1010@gmail.com>
2023-01-13 14:27:15 +0200

    created a collision between the paddle and the ball. changed the background style

--- game_pong/paddle.js
+++ game_pong/paddle.js
@@ -13,6 +13,10 @@ export default class Paddle {
         this.paddleElem.style.setProperty("--position", value);
     }
 
+    rect() {
+       return this.paddleElem.getBoundingClientRect()
+    }
+
     reset() {
         this.position = 50;
     }
