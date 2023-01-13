e656c9f88ec1b5b7c745e6e28cf800128f007ff8
TangiresH <dimon4ik1010@gmail.com>
2023-01-13 16:49:41 +0200

    created a menu on the site

---
+++ site/index.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll('.menu a');
+const ind = document.querySelector('.indicator');
+
+links.forEach(item => {
+    item.addEventListener('click', (e) => {
+        moveInd(e.target);
+    });
+});
+
+function moveInd(el) {
+    ind.style.width = `${el.offsetWidth}px`;
+    ind.style.left = `${el.offsetLeft}px`;
+}
