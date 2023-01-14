eb555498ca156cbb6e0b448919fd5dc5121ebbc6
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-01-14 02:46:39 +0200

    improved the project

--- site/index.js
+++ site/index.js
@@ -1,13 +1,14 @@
+'use strict';
 const links = document.querySelectorAll('.menu a');
 const ind = document.querySelector('.indicator');
 
 links.forEach(item => {
-    item.addEventListener('click', (e) => {
-        moveInd(e.target);
-    });
+  item.addEventListener('click', e => {
+    moveInd(e.target);
+  });
 });
 
 function moveInd(el) {
-    ind.style.width = `${el.offsetWidth}px`;
-    ind.style.left = `${el.offsetLeft}px`;
+  ind.style.width = `${el.offsetWidth}px`;
+  ind.style.left = `${el.offsetLeft}px`;
 }
